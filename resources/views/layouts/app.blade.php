567db39f7faa45e3e7a8796f4d478d481655b49d
Your Name <you@example.com>
2026-05-01 13:02:53 +0100

    final change in login and register

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,8 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Online Vegg') }}</title>
+    <!-- <title>{{ config('app.name', 'Online Vegg') }}</title> -->
+    <title>Online Vegg</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
@@ -29,7 +30,8 @@
 
                     <!-- Branding Image -->
                     <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Online Vegg') }}
+                            Online Vegg
+                        <!-- {{ config('app.name', 'Online Vegg') }} -->
                     </a>
                 </div>
 
